0a33f5db3f0640f79724b49c670625e33a6f38be
Jeewoo Lee <jlee@lcs.on.ca>
2020-05-09 16:57:22 +0900

    Initial Commit

---
+++ sortingAlogorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sortingAlogorithm
+//
+//  Created by Lee, Jeewoo on 2020-05-09.
+//  Copyright © 2020 Lee, Jeewoo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
