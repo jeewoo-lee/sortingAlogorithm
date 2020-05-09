510fe3e502439eebd4dd0fdcca708b4ba87ef9f0
Jeewoo Lee <jlee@lcs.on.ca>
2020-05-09 18:14:44 +0900

    Finished "Selection" sorting algorithm

--- sortingAlogorithm/main.swift
+++ sortingAlogorithm/main.swift
@@ -8,6 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
-//
+print("Selection Sorting")
+
+//Create an empty array
+var dataSet: [Int] = []
+
+//Populate an array to sort
+for _ in 1...10 {
+    dataSet.append(Int.random(in: 1...100))
+}
+
+//Print the unsorted array
+print("Unsorted: ")
+print(dataSet)
+
+for i in 0...dataSet.count - 1 {
+//    let minimum = dataSet[i]
+    for j in i...dataSet.count - 1 {
+        if dataSet[i] > dataSet[j] {
+            let temporaryValue = dataSet[j]
+            dataSet[j] = dataSet[i]
+            dataSet[i] = temporaryValue
+        }
+    }
+}
+
+print("Sorted: ")
+print(dataSet)
+
 
